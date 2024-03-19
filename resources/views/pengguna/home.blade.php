4e0684d3d6396dd4b8cc639e6ed832f7504ac551
mr-lunart <dewanata_85@yahoo.com>
2024-03-20 02:08:40 +0700

    add dummy slider for development

--- resources/views/pengguna/home.blade.php
+++ resources/views/pengguna/home.blade.php
@@ -56,8 +56,8 @@
         <div class="container d-flex justify-content-start">
             <h1 class="text-primary fw-bold">Hi! Selamat Datang di <br>Airnav Assist</h1>
         </div>
-    </header>    
-    
+    </header>
+
     <!-- SLIDER -->
     <div class="container">
         <section class="mt-5 pt-3 pb-3 slider-nav">
@@ -68,15 +68,33 @@
                 <div class="slider-wrapper w-100">
                     <div class="d-flex flex-column">
                         <div id="owl-carousel" class="">
-                            @foreach ($dataArtikel as $item)
-                                <div class="slide-item">
-                                    <div class="slide card" style="background-image: url('{{ asset('src/img/artikel1.jpeg') }}')">
-                                        <h4 class="text-center">{{ $item->judul }}</h4>
-                                        <div class="des">{{ $item->deskripsi }}</div>
+                            <div class="slide-item">
+                                <div class="slide card" style="background-image: url('{{ asset('src/img/artikel1.jpeg') }}'); background-size: cover;">
+                                    <h4 class="text-center">Test 1</h4>
+                                    <div class="des">Teks deskripsi sederhana untuk card div dan memeriksa lebar width sudah sesuai apa belum</div>
+                                    <hr>
+                                    <button class="btn btn-light">See More</button>
+                                </div>
+                            </div>
+                            <div class="slide-item">
+                                <div class="position-relative">
+                                    <div class="slide card" style="background-image: url('{{ asset('src/img/artikel1.jpeg') }}'); background-size: cover;">
+                                        <h4 class="text-center" >Test 2</h4>
+                                        <div class="des">Teks deskripsi sederhana untuk card div dan memeriksa lebar width sudah sesuai apa belum</div>
                                         <hr>
                                         <button class="btn btn-light">See More</button>
                                     </div>
                                 </div>
+                            </div>
+                            @foreach ($dataArtikel as $item)
+                            <div class="slide-item">
+                                <div class="slide card" style="background-image: url('{{ asset('src/img/artikel1.jpeg') }}')">
+                                    <h4 class="text-center">{{ $item->judul }}</h4>
+                                    <div class="des">{{ $item->deskripsi }}</div>
+                                    <hr>
+                                    <button class="btn btn-light">See More</button>
+                                </div>
+                            </div>
                             @endforeach
                         </div>
                         <div id="owl-nav" class="align-self-center mt-3">
