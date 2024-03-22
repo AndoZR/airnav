aa717aaad6071af3ca809e5ff762ba27f1fe8ded
mr-lunart <dewanata_85@yahoo.com>
2024-03-23 01:29:12 +0700

    fix slider, logo, visi misi, organisasi

--- resources/views/pengguna/home.blade.php
+++ resources/views/pengguna/home.blade.php
@@ -14,10 +14,10 @@
 </head>
 
 <body>
-    <div class="position-relative top-0">
+    <div class="position-relative bottom-0">
         <!-- NAVBAR -->
         <nav class="navbar navbar-expand-lg" style="background-color: #49548C">
-            <div class="container-fluid">
+            <div class="container">
                 <a class="navbar-brand" href="#">
                     <img src="{{ asset('src/img/logoAirNav.png') }}" alt="Airnav Assist" height="40">
                 </a>
@@ -46,7 +46,7 @@
                         </li>
                     </ul>
                     @if (Auth::guard('web')->check())
-                        <a href="{{ route('logout') }}" class="btn btn-sm btn-outline-light" type="button">Log Out</a>
+                    <a href="{{ route('logout') }}" class="btn btn-sm btn-outline-light" type="button">Log Out</a>
                     @endif
                 </div>
             </div>
@@ -54,53 +54,35 @@
     </div>
 
     {{-- MASTERHEAD --}}
-    <header class="masthead transBg">
+    <header class="masterhead">
         <div class="container d-flex justify-content-start">
-            <h1 class="text-primary fw-bold">Hi! Selamat Datang di <br>Airnav Assist</h1>
+            <h1 class="text-primary fw-bold" style="text-shadow: 0px 0px 10px white, 2px 2px 3px white, 5px 5px 20px white;">Hi! Selamat Datang di <br> Airnav Assist</h1>
         </div>
     </header>
-    
+
     <!-- SLIDER -->
-    <div class="container">
+    <div class="container-fluid">
         <section class="mt-5 pt-3 pb-3 slider-nav">
             <div class=" d-flex flex-row justify-content-between">
-                <div class="display-slider">
-                    <h3><b>Artikel Edukasi</b></h3>
+                <div id="display-slider">
+                    <h3><strong>Artikel Edukasi</strong></h3>
                     <p>Baca berbagai informasi terbaru bandara untuk meningkatkan pemahamanmu</p>
                 </div>
                 <div class="slider-wrapper w-100">
                     <div class="d-flex flex-column">
                         <div id="owl-carousel" class="">
-                            <div class="slide-item">
-                                <div class="slide card" style="background-image: url('{{ asset('src/img/artikel1.jpeg') }}'); background-size: cover;">
-                                    <h4 class="text-center">Test 1</h4>
-                                    <div class="des">Teks deskripsi sederhana untuk card div dan memeriksa lebar width sudah sesuai apa belum</div>
-                                    <hr>
-                                    <button class="btn btn-light">See More</button>
-                                </div>
-                            </div>
-                            <div class="slide-item">
-                                <div class="position-relative">
-                                    <div class="slide card" style="background-image: url('{{ asset('src/img/artikel1.jpeg') }}'); background-size: cover;">
-                                        <h4 class="text-center" >Test 2</h4>
-                                        <div class="des">Teks deskripsi sederhana untuk card div dan memeriksa lebar width sudah sesuai apa belum</div>
-                                        <hr>
-                                        <button class="btn btn-light">See More</button>
-                                    </div>
-                                </div>
-                            </div>
                             @foreach ($dataArtikel as $item)
                             <div class="slide-item">
-                                <div class="slide card" style="background-image: url('{{ asset('src/img/artikel1.jpeg') }}')">
-                                    <h4 class="text-center">{{ $item->judul }}</h4>
-                                    <div class="des">{{ $item->deskripsi }}</div>
-                                    <hr>
-                                    <button class="btn btn-light">See More</button>
+                                <div class="slide" style="background-image: linear-gradient(to bottom, #ffffff, transparent 150%), url('{{ asset('src/img/artikel1.jpeg') }}');  background-repeat: no-repeat; background-attachment: scroll; background-position: top; background-size: 300%;">
+                                    <h4 class="text-center fw-bold">{{ $item->judul }}</h4>
+                                    <div class="des fw-normal text-body-secondary">{{ $item->deskripsi }}</div>
+                                    <br>
+                                    <button class="btn btn-light fw-semibold">Selengkapnya...</button>
                                 </div>
                             </div>
                             @endforeach
                         </div>
-                        <div id="owl-nav" class="align-self-center mt-3">
+                        <div id="owl-nav" class="align-self-center mt-3 slider-button">
                             <button type="button" class="owl-prev mx-5 btn btn-light"><i class="fa-solid fa-arrow-left"></i></button>
                             <button type="button" class="owl-next mx-5 btn btn-light"><i class="fa-solid fa-arrow-right"></i></button>
                         </div>
@@ -111,50 +93,58 @@
     </div>
 
     {{-- BACKGROUND II --}}
-    <header class="background transBg">
-        <div class="container d-flex justify-content-start">
-            
+    <header class="background transBg container-fluid">
+        <div class="container">
         </div>
     </header>
 
     {{-- VISI MISI --}}
-    <div class="row">
-        <div class="col-sm-4">
-          <div class="card h-100 w-100 shadow ms-5">
-            <div class="card-body">
-              <h5 class="card-title">Visi</h5>
-              <p class="card-text">Menjadi penyedia jasa navigasi penerbangan bertaraf internasional</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-6 position-relative">
+                <div id="visi" class="card position-absolute shadow">
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold">Visi</h5>
+                        <p class="card-text ">Menjadi penyedia jasa navigasi penerbangan bertaraf internasional</p>
+                    </div>
+                </div>
+
+                <div id="misi" class="card position-absolute shadow">
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold">Misi</h5>
+                        <p class="card-text">Menyediakan layanan navigasi penerbangan yang mengutamakan keselamatan,
+                            efisiensi penerbangan dan ramah lingkungan demi memenuhi ekspetasi pengguna jasa</p>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-        <div class="col-sm-4">
-          <div class="card h-100 w-100 shadow cardVisiMisi">
-            <div class="card-body">
-              <h5 class="card-title">Misi</h5>
-              <p class="card-text">Menyediakan layanan navigasi penerbangan yang mengutamakan keselamatan,
-                efisiensi penerbangan dan ramah lingkungan demi memenuhi ekspetasi pengguna jasa</p>
+            <div class="col-6 position-relative logo">
+                <div class="d-flex flex-column justify-content-end align-items-center">
+                    <h2 class="text-end fw-bold" style=" color:#49548C;">Airnav Indonesia</h2>
+                    <img class="logoVisiMisi" src="{{ asset('src/img/logo.png') }}" alt="Logo">
+                    <h2 class="text-end fw-bold" style=" color:#49548C;">Cabang Tanjung Pinang</h2>
+
+                </div>
             </div>
-          </div>
-        </div>
-        <div class="col-sm-4">
-            <img class="img-fluid logoVisiMisi" src="{{ asset('src/img/logo.png') }}" alt="Logo">
         </div>
     </div>
 
+
     {{-- BACKGROUND III --}}
-    <header class="background transBg position-relative">
-        <div class="container">
-            <div class="row">
-                <div class="d-flex justify-content-center align-items-center">
-                    <h1 class="text-primary fw-bold" style="margin-top: -30rem;">Struktur Organisasi Cabang<br>Tanjung Pinang<br>Bagang Manajerial</h1>
+    <div class="endbackground container-fluid">
+    </div>
+
+    <div class="lastbackground">
+        <div class="container text-center">
+            <div class="row" style="padding-bottom: 10rem;">
+                <div class="d-flex justify-content-center align-items-center" style="margin-top: 5rem;">
+                    <h1 class="text-primary fw-bold" style="margin-top: -20rem;">Struktur Organisasi Cabang<br>Tanjung Pinang<br>Bagang Manajerial</h1>
                 </div>
-                <div class="d-flex justify-content-center align-items-center">
+                <div class="d-flex justify-content-center align-items-center" style="margin-bottom: 5rem;">
                     <img src="{{ asset('src/img/bagan.png') }}" alt="Bagan" style="max-width: 100%;">
                 </div>
             </div>
         </div>
-    </header>
-    
+    </div>
     {{-- FOOTER --}}
     <footer class="text-center text-lg-start text-white" style="background-color: #3e4551">
         <!-- Grid container -->
@@ -173,26 +163,26 @@
                         </p>
                     </div>
                     <!--Grid column-->
-        
+
                     <!--Grid column II-->
                     <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
                         <h5 class="text-uppercase">LAYANAN</h5>
-            
+
                         <ul class="list-unstyled mb-0">
                             <li>
-                            <a href="#!" class="text-white">Beranda</a>
+                                <a href="#!" class="text-white">Beranda</a>
                             </li>
                             <li>
-                            <a href="#!" class="text-white">Artikel</a>
+                                <a href="#!" class="text-white">Artikel</a>
                             </li>
                             <li>
-                            <a href="#!" class="text-white">Pembelajaran</a>
+                                <a href="#!" class="text-white">Pembelajaran</a>
                             </li>
                             <li>
-                            <a href="#!" class="text-white">Organisasi Cabang</a>
+                                <a href="#!" class="text-white">Organisasi Cabang</a>
                             </li>
                             <li>
-                            <a href="#!" class="text-white">Akun</a>
+                                <a href="#!" class="text-white">Akun</a>
                             </li>
                         </ul>
                     </div>
@@ -200,71 +190,41 @@
 
                     <!--Grid column III-->
                     <div class="col-lg-4 col-md-6 mb-4 mb-md-0 d-flex justify-content-center align-items-center">
-                        <img class="w-100" src="{{ asset('src/img/logoairnav.png') }}" alt="logo">
-                    </div>                    
+                        <img class="w-75" src="{{ asset('src/img/logoAirNav.png') }}" alt="logo">
+                    </div>
                     <!--Grid column-->
-                    
+
                 </div>
                 <!--Grid row-->
             </section>
             <!-- Section: Links -->
-    
-            <hr class="mb-4"/>
-    
+
+            <hr class="mb-4" />
+
             <!-- Section: Social media -->
             <section class="mb-4 text-center">
-            <!-- Facebook -->
-            <a
-                class="btn btn-outline-light btn-floating m-1"
-                href="#!"
-                role="button"
-                ><i class="fab fa-facebook-f"></i
-                ></a>
-    
-            <!-- Twitter -->
-            <a
-                class="btn btn-outline-light btn-floating m-1"
-                href="#!"
-                role="button"
-                ><i class="fab fa-twitter"></i
-                ></a>
-    
-            <!-- Google -->
-            <a
-                class="btn btn-outline-light btn-floating m-1"
-                href="#!"
-                role="button"
-                ><i class="fab fa-google"></i
-                ></a>
-    
-            <!-- Instagram -->
-            <a
-                class="btn btn-outline-light btn-floating m-1"
-                href="#!"
-                role="button"
-                ><i class="fab fa-instagram"></i
-                ></a>
-    
-            <!-- Linkedin -->
-            <a
-                class="btn btn-outline-light btn-floating m-1"
-                href="#!"
-                role="button"
-                ><i class="fab fa-linkedin-in"></i
-                ></a>
-    
-            <!-- Github -->
-            <a
-                class="btn btn-outline-light btn-floating m-1"
-                href="#!"
-                role="button"
-                ><i class="fab fa-github"></i
-                ></a>
+                <!-- Facebook -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+
+                <!-- Twitter -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+
+                <!-- Google -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
+
+                <!-- Instagram -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+
+                <!-- Linkedin -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+
+                <!-- Github -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-github"></i></a>
             </section>
             <!-- Section: Social media -->
         </div>
         <!-- Grid container -->
-  
+
         <!-- Copyright -->
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
             © 2024 Copyright:
@@ -280,11 +240,9 @@
 <script>
     var slider = tns({
         container: '#owl-carousel',
-        mode: 'carousel',
         mouseDrag: true,
         nav: false,
         axis: "horizontal",
-        // gutter: 50,
         autoWidth: true,
         center: true,
         controlsContainer: document.getElementById('owl-nav'),
