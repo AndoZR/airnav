1dc5ebb546b2de3be425f93253dcc542d078c9c2
mr-lunart <dewanata_85@yahoo.com>
2024-04-17 16:52:04 +0700

    fix conflict with git commit
    This reverts commit cee8fdf84147f53a9a35687ceead287fccbd8fca.

--- resources/views/pengguna/pembelajaran.blade.php
+++ resources/views/pengguna/pembelajaran.blade.php
@@ -1,6 +1,7 @@
 @extends('pengguna.app')
 @section('tab', 'Pembelajaran')
 
+<<<<<<< HEAD
 @section('content')
 {{-- MASTERHEAD --}}
 <header class="masterheadBelajar">
@@ -200,3 +201,31 @@ document.addEventListener("DOMContentLoaded", function() {
 });    
 </script> 
 @endpush
+=======
+@section( 'content' )
+    {{-- MASTERHEAD --}}
+    <header class="masterhead">
+        <div class="container d-flex justify-content-between align-items-center">
+            <h1 class="text-primary fw-bolder" style="text-shadow: 0px 0px 10px white, 2px 2px 3px white, -2px -2px 5px white , 5px 5px 20px white;"><strong>HANG NADIM <br> TOWER</strong></h1>
+        </div>
+    </header>
+   
+
+    {{-- BACKGROUND III --}}
+    <div class="endbackground container-fluid"></div>
+
+    <div class="lastbackground">
+        <div class="container text-center">
+            <div class="row" style="padding-bottom: 3rem;">
+                <div class="d-flex justify-content-center align-items-center" style="margin-top: 5rem;">
+                    <h1 class="text-primary fw-bold" style="margin-top: -20rem;">Struktur Organisasi Cabang<br>Tanjung Pinang<br>(Bagan Managerial)</h1>
+                </div>
+                <div class="d-flex justify-content-center align-items-center" style="margin-bottom: 5rem;">
+                    <img src="{{ asset('src/img/bagan.png') }}" alt="Bagan" style="max-width: 100%;">
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
+>>>>>>> parent of cee8fdf (add slider for video pembelajaran)
