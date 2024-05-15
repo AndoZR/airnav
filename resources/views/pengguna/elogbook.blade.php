ce2cd9f2467dcb16873143e155adf6f2cb123801
mr-lunart <dewanata_85@yahoo.com>
2024-05-15 22:52:34 +0700

    add table to e logbook

--- resources/views/pengguna/elogbook.blade.php
+++ resources/views/pengguna/elogbook.blade.php
@@ -3,14 +3,49 @@
 
 @section( 'content' )
 {{-- MASTERHEAD --}}
-<header class="masterheadBelajar">
-    <div class="container d-flex justify-content-between align-items-center">
-        <h1 class="text-primary fw-bolder" style="text-shadow: 0px 0px 10px white, 2px 2px 3px white, -2px -2px 5px white , 5px 5px 20px white;"><strong>ELOGBOOK</strong></h1>
-    </div>
+<header class="">
+<div></div>
 </header>
+<div class="container">
+    <div class="table-responsive-sm">
+        <table class="table table-sm table-bordered">
+            <thead class="text-center">
+                <tr class="">
+                    <th scope="col" rowspan="2"> <small>Date</small></th>
+                    <th scope="col" colspan="3"><small>Morning</small></th>
+                    <th scope="col" colspan="3"><small>Afternoon</small></th>
+                    <th scope="col" colspan="3"><small>Night</small></th>
+                    <th scope="col" colspan="6"><small>Unit</small></th>
+                    <th scope="col" rowspan="2"><small>Option</small></th>
+                </tr>
+                <tr class="">
+                    <th scope="col"><small>CTR</small></th>
+                    <th scope="col"><small>ASS</small></th>
+                    <th scope="col"><small>REST</small></th>
+                    <th scope="col"><small>CTR</small></th>
+                    <th scope="col"><small>ASS</small></th>
+                    <th scope="col"><small>REST</small></th>
+                    <th scope="col"><small>CTR</small></th>
+                    <th scope="col"><small>ASS</small></th>
+                    <th scope="col"><small>REST</small></th>
+                    <th scope="col"><small>ADC</small></th>
+                    <th scope="col"><small>APP</small></th>
+                    <th scope="col"><small>APP SURV</small></th>
+                    <th scope="col"><small>COMB ADC/APP</small></th>
+                    <th scope="col"><small>ACC</small></th>
+                    <th scope="col"><small>ACC SURV</small></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
 
 @push('scripts')
-    <script>
-    </script>
+<script>
+</script>
 @endpush
